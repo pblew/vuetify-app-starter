996738196d08cb0da731ac4b0cabfdec49e4bfd2
Paul Blew <paul.blew@gmail.com>
2025-06-21 15:09:01 +0100

    Updated dependencies and ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
 import pluginVue from "eslint-plugin-vue";
-import vueTsEslintConfig from "@vue/eslint-config-typescript";
+import { defineConfigWithVueTs, vueTsConfigs } from "@vue/eslint-config-typescript";
 import skipFormatting from "@vue/eslint-config-prettier/skip-formatting";
 
-export default [
+export default defineConfigWithVueTs(
     {
-        files: ["**/*.ts", "**/*.vue"],
+        files: ["**/*.{ts,vue}"],
         rules: {
             "@typescript-eslint/explicit-module-boundary-types": 0,
             "@typescript-eslint/no-unused-vars": [
@@ -21,10 +21,7 @@ export default [
     {
         ignores: ["**/dist/**", "**/dist-ssr/**", "**/coverage/**"],
     },
-    ...pluginVue.configs["flat/recommended"],
-    ...vueTsEslintConfig({
-        // see: https://typescript-eslint.io/users/configs/#recommended-configurations
-        extends: ["recommended"],
-    }),
+    pluginVue.configs["flat/recommended"],
+    vueTsConfigs.recommended,
     skipFormatting,
-];
+);
